1d83393b92262bf8bbed53895514893f5facd9a1
Christy Eang <christyeang@yahoo.com>
2021-04-14 15:54:58 -0700

    added the navbars to the pages(again) and added routermodule to navbar

--- RTDMobileApp/src/app/components/components.module.ts
+++ RTDMobileApp/src/app/components/components.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import {NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import {NavComponent} from './nav/nav.component';
 
 
 @NgModule({
     imports: [
+        RouterModule,
         IonicModule,
         CommonModule,
         FormsModule
