42d4b59c19f1938317fd1a728cfff1d927291130
Christy Eang <christyeang@yahoo.com>
2021-04-15 11:59:59 -0700

    3 buttons in favorites up, nothing is hooked up yet though.

--- RTDMobileApp/src/app/pages/favorites/favorites-main/favorites-main.page.ts
+++ RTDMobileApp/src/app/pages/favorites/favorites-main/favorites-main.page.ts
@@ -6,15 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorites-main.page.scss'],
 })
 export class FavoritesMainPage implements OnInit {
+  favorite = "1000 March Lane";
+  iconName = "home";
+  favBus = "40 Pacific Corridor: DTC - HTS";
+  oneRidePrice = "1.50";
+  dayPassPrice = "4.00";
+  monthPassPrice = "64.00";
 
   constructor() { }
 
   ngOnInit() {
   }
-  favorite = "1000 March Lane";
-  iconName = "home";
-  favBus = "40 Pacific Corridor: DTC - HTS";
-
   editMe(){
     // be able to edit the button text inside, maybe make it an input box?
     alert("my edit button has been clicked");
