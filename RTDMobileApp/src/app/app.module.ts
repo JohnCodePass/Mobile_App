f952963d9a69786b74bcb05e82acce6f55279563
Kenneth Martinez <krossmartinez@gmail.com>
2021-04-15 09:46:14 -0700

    bootsrap and cards

--- RTDMobileApp/src/app/app.module.ts
+++ RTDMobileApp/src/app/app.module.ts
@@ -7,11 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavComponent } from './components/nav/nav.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, NgbModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- RTDMobileApp/src/app/pages/rider-alerts/rider-alerts.module.ts
+++ RTDMobileApp/src/app/pages/rider-alerts/rider-alerts.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { RiderAlertsPageRoutingModule } from './rider-alerts-routing.module';
+import  {ComponentsModule} from '../../components/components.module'
 
 import { RiderAlertsPage } from './rider-alerts.page';
 
@@ -13,7 +14,8 @@ import { RiderAlertsPage } from './rider-alerts.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    RiderAlertsPageRoutingModule
+    RiderAlertsPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [RiderAlertsPage]
 })
