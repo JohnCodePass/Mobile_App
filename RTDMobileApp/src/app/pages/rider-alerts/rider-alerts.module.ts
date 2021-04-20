19116df74b20431001774d0f1b6595dcd76b479a
Trevor Womack Jr <Twomack6823@gmail.com>
2021-04-20 14:07:53 -0700

    fixed errors

--- RTDMobileApp/src/app/pages/rider-alerts/rider-alerts.module.ts
+++ RTDMobileApp/src/app/pages/rider-alerts/rider-alerts.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { RiderAlertsPageRoutingModule } from './rider-alerts-routing.module';
-import  {ComponentsModule} from '../../components/components.module'
-
 import { RiderAlertsPage } from './rider-alerts.page';
 import { ComponentsModule } from 'src/app/components/components.module';
 
