42e136e12aabb51dbb4d85ac60fdaf0c0676006d
Alex A <alexato0731@gmail.com>
2021-04-20 16:23:34 -0700

    adding google map component

---
+++ RTDMobileApp/src/app/pages/google-map/google-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+declare var google: any;
+
+@Component({
+  selector: 'app-google-map',
+  templateUrl: './google-map.component.html',
+  styleUrls: ['./google-map.component.scss'],
+})
+export class GoogleMapComponent{
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
